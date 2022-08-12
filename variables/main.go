7170fdbe8cc815687689d902858fb7b88edd46ba
Hugo Souza <hugoedson2019@gmail.com>
2022-08-12 10:35:25 -0300

    Update main.go

--- variables/main.go
+++ variables/main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var idade int = 17
-	var nome string = "Hugo Souza"
-	fmt.Print("Name: ", nome, "\n", "Age: ", idade)
+	//var (
+	//	idade = 17
+	//	nome  = "Hugo Souza"
+	//)
+
+	//var altura, idade = 160, 17
+	//var nome = "Hugo Souza"
+
+	idade, altura, nome := 17, 160, "Hugo Souza"
+
+	//var idade int = 17
+	//var nome string = "Hugo Souza"
+	fmt.Print("Name: ", nome, "\nAge: ", idade, "\nHeight: ", altura)
 }
